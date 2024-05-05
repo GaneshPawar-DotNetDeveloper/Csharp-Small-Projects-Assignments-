88d98e1837fba36e170950eb27e4a9a33c86db2b
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-05 22:39:49 +0530

    Add files via upload

---
+++ Armstrong_Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Armstrong_Number
+{
+    class Program
+    {
+        public void Armstrong()
+        {
+            Console.WriteLine("Enter Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int rem;
+            int sum = 0;
+            int temp = number;
+
+            while (number > 0)
+            {
+                rem = number % 10;
+                sum = sum + (rem * rem * rem);
+                number = number / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("Number is palindrom");
+            }
+            else
+            {
+
+                Console.WriteLine("Number is not Palindrom");
+            }
+        }
+    }
+}
