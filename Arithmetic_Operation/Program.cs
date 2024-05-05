dc0e2986307f132f01c1ecaec3dd63bc471241fc
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-05 22:38:39 +0530

    Add files via upload

---
+++ Arithmetic_Operation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arithmetic_Operation
+{
+    class Program
+    {
+        //AddSubMultiDiv 2//
+        public void AddSubMultiDiv(int a, int b)
+        {
+            int addition = a + b;
+            Console.WriteLine("Addition=" + addition);
+            int substraction = a - b;
+            Console.WriteLine("substraction=" + substraction);
+            int multiplication = a * b;
+            Console.WriteLine("multiplication=" + multiplication);
+            int Division = a / b;
+            Console.WriteLine("Division=" + Division);
+
+        }
+    }
+}
