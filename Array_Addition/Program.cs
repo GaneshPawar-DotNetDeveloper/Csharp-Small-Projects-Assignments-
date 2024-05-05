e0bb00898b37612d08d8e1952f01624c58fe4956
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-05 22:40:34 +0530

    Add files via upload

---
+++ Array_Addition/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Array_Addition
+{
+    class Program
+    {
+        public void ArrayAddition()
+        {
+            int[] number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int sum = 0;
+            int i;
+            for (i = 0; i <= number.Length - 1; i++)
+            {
+                sum = sum + number[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
