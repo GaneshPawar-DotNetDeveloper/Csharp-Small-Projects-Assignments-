6b5f7f0732d6b07d7016b5b3e2d64d2d772d2f39
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-06 10:52:10 +0530

    Add files via upload

---
+++ Highest_Element_From_Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Highest_Element_From_Array
+{
+    class Program
+    {
+        //higest string from array
+        public void HigestStringFromArray()
+        {
+            Console.WriteLine("Please Enter String ");
+
+            string str = Console.ReadLine();
+            string[] strs = str.Split(new[] { " " }, StringSplitOptions.None);
+            string word = string.Empty;
+            int tr = 0;
+
+            foreach (string s in strs)
+            {
+                if (s.Length > tr)
+                {
+                    word = s;
+                    tr = s.Length;
+                }
+            }
+            Console.WriteLine(word);
+        }
+
+
+    }
+}
+
