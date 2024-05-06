173ef1bb4a6b5e01d609e2513e35c304c01d4483
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-06 10:51:40 +0530

    Add files via upload

---
+++ Fibonanci_series/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonanci_series
+{
+    class Program
+    {
+        public void Fibonanciseries()
+        {
+            Console.WriteLine("Enter num1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter num2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int c;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                c = num1 + num2;
+                Console.WriteLine(c);
+                num1 = num2;
+                num2 = c;
+            }
+        }
+
+    }
+}
