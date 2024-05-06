98219f82d4034f3192ea23a5691f7b73f89c5871
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-06 10:50:34 +0530

    palindrome series

---
+++ Assignment_Questions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Assignment_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            //Palindrom String//
+
+            Console.WriteLine("Enter String");
+            string str = Console.ReadLine();
+            string temp = string.Empty;
+
+            for (int i = (str.Length - 1); i >= 0; i--)
+            {
+                temp = temp + str[i];
+            }
+            if (str == temp)
+            {
+                Console.WriteLine("PAlindrom");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrom");
+            }
+
+        }
+
+        
+    }
+}
+
+
