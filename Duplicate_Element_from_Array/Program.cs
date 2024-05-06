d7039c1bb3e67c089055457a148462f8da7ba14d
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-06 10:51:05 +0530

    Add files via upload

---
+++ Duplicate_Element_from_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Duplicate_Element_from_Array
+{
+    class Program
+    {
+        //Duplicate Elements From Array
+        public void DuplicateElementFromArray()
+        {
+            int[] number = new int[] { 10, 20, 10, 50, 40, 40, 60, 70, 20 };
+
+
+            for (int i = 0; i <= (number.Length); i++)
+            {
+                for (int j = i + 1; j < (number.Length); j++)
+                {
+                    if (number[i] == number[j])
+                    {
+
+                        Console.WriteLine(number[i]);
+                    }
+                }
+            }
+        }
+
+    }
+}
