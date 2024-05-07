a59c067d1c548602841e81904b5bb6586152a6b5
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-07 18:49:15 +0530

    Add files via upload

---
+++ Nth_Highest_Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nth_Highest_Number
+{
+    class Program
+    {
+        // Nth Higest Number  from array//
+        public void NthHighestNumberFromArray()
+        {
+
+
+            int[] Number = { 10, 20, 30, 40, 50, 45, 65, 100, 95 };
+            Array.Sort(Number);
+            Console.WriteLine("Higest Number =" + Number[Number.Length - 1]);
+
+        }
+
+    }
+}
