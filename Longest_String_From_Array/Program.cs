0653149f70d6fca103f15a4b9d92ed7f7e056051
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-07 18:48:11 +0530

    Add files via upload

---
+++ Longest_String_From_Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Longest_String_From_Array
+{
+    class Program
+    {
+        //Longest String From Array//
+        public void HigestStringFromArray()
+        {
+            string str = "Please waer Mask and Sanitise hand regularly";
+            //string[] strs = str.Split(new[] { " " }, StringSplitOptions.None);
+            string[] strs = str.Split(new[] { " " }, StringSplitOptions.None);
+            string word = string.Empty;
+            int tr = 0;
+
+            foreach (string s in strs)
+            {
+                if (s.Length > tr)
+                {
+                    word = s;
+                    tr = s.Length;
+                }
+            }
+            Console.WriteLine(word);
+
+        }
+    }
+}
