61224a2ea58ed3ad2bc41c3ce3afe82f74273845
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-07 18:47:04 +0530

    Add files via upload

---
+++ Highest_Number_In_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Highest_Number_In_Array
+{
+    class Program
+    {
+        //Higest Number From Array
+        public void HigestElementFromArray()
+        {
+            int[] numbers = new int[] { 10, 50, 100, 90, 70, 80, 60, 30, 40, 20 };
+            Array.Sort(numbers);
+            Console.WriteLine("Higest Number In Array is=" + numbers[numbers.Length - 1]);
+
+        }
+    }
+}
