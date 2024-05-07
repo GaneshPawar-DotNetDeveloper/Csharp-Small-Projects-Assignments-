61dbc80cfc99d854d67e445dec37d3dff7dd8b02
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-07 18:47:36 +0530

    Add files via upload

---
+++ K_th_Largest_element_from_Array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace K_th_Largest_element_from_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] number = new int[] { 10, 30, 20, 90, 40, 50, 60 };
+            Array.Sort(number);
+            int b = Convert.ToInt32(Console.ReadLine());
+            int a = number[number.Length - b];
+            Console.WriteLine("Largest Element in Array is=" + a);
+        }
+    }
+}
