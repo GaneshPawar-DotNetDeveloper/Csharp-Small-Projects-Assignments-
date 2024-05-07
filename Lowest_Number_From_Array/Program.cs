5addf52ed8be442280070e10d45bd1ab9795072d
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-07 18:48:39 +0530

    Add files via upload

---
+++ Lowest_Number_From_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lowest_Number_From_Array
+{
+    class Program
+    {
+        //Lowest Number From Array
+        public void LowestElementFromArray()
+        {
+            int[] numbers = new int[] { 10, 50, 100, 90, 70, 80, 60, 30, 40, 20 };
+            Array.Sort(numbers);
+            Console.WriteLine("Lowest Number In Array is=" + numbers[0]);
+
+        }
+    }
+}
