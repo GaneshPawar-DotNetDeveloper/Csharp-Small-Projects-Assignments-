a8ce4c39bf2ff231eab8bb7be5fd39eb0d7ea343
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-08 21:54:54 +0530

    Add files via upload

---
+++ Odd_Even/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Odd_Even
+{
+    class Program
+    {
+        public void Assignmenttask11()
+        {
+            Console.WriteLine("Enter Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string oddnum = string.Empty;
+            string EvenNum = string.Empty;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    EvenNum = EvenNum + ' ' + i;
+                }
+                else
+                {
+                    oddnum = oddnum + ' ' + i;
+                }
+            }
+            Console.WriteLine(oddnum + " " + EvenNum);
+        }
+    }
+}
