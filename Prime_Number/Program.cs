d7a80a1825378a988d2e5f49225c0a75e8f12b59
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-08 21:56:19 +0530

    Add files via upload

---
+++ Prime_Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        public void prime()
+        {
+            Console.WriteLine("Enter num1");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count += 1;
+                }
+
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+        }
+    }
+}
