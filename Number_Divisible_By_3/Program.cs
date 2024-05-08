7027780653375b654c1a1eca51ce29d5f5dcb125
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-08 21:55:28 +0530

    Add files via upload

---
+++ Number_Divisible_By_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Number_Divisible_By_3
+{
+    class Program
+    {
+        //Three if Number is divisible by 3//
+        public void PrintLetterIfNumberDivisible()
+        {
+            Console.WriteLine("Enter Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Number is Divisible by Three  and Five");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Number is divisible by 5");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Number is Divisible by Three");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Divisible by Three and Five");
+            }
+        }
+    }
+}
