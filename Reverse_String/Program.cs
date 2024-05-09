b89c3b90f6931e92ca351d94e51bb9325f65e266
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-09 21:21:46 +0530

    Add files via upload

---
+++ Reverse_String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        //Reverse String//
+        public void ReverseString()
+        {
+            Console.WriteLine("Enter String");
+            string Str = Console.ReadLine();
+            string temp = string.Empty;
+
+            for (int i = (Str.Length - 1); i >= 0; i--)
+            {
+                temp = temp + Str[i];
+            }
+            Console.WriteLine(temp);
+
+        }
+    }
+}
