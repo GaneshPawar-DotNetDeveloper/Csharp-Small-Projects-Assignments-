071965507c4a0db9ce859381a1229eb7a3306a37
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-09 21:20:47 +0530

    Add files via upload

---
+++ Reverse_array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Reverse_array
+{
+    class Program
+    {
+        //Reverse Array//
+        public void ReverseArray()
+        {
+            int[] numbers = new int[] { 10, 20, 30, 40, 50, 60 };
+
+
+            for (int i = (numbers.Length - 1); i >= 0; i--)
+            {
+                Console.Write(" " + numbers[i]);
+            }
+
+        }
+    }
+}
