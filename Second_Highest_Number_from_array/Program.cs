719d3c691ba9c7c88200a84c85d8c6f54d4f9c47
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-09 21:23:33 +0530

    Add files via upload

---
+++ Second_Highest_Number_from_array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Second_Highest_Number_from_array
+{
+    class Program
+    {
+        //2nd higest number from Array//
+        public void HighestNumberFromArray()
+        {
+            int[] Number = { 10, 20, 30, 40, 50, 45, 65, 100, 95 };
+            Array.Sort(Number);
+            Console.WriteLine("Higest Number =" + Number[Number.Length - 2]);
+
+        }
+    }
+}
