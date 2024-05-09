8b23959803e41bf9a89f923364ef0e85402f98df
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-09 21:21:17 +0530

    Add files via upload

---
+++ Reverse_Sentence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reverse_Sentence
+{
+    class Program
+    {
+        //Reverse Sentence //
+        public void reversesentences()
+        {
+            string str = " how are you";
+            string[] words = str.Split(new[] { " " }, StringSplitOptions.None);
+            string word = string.Empty;
+
+            for (int i = (words.Length - 1); i >= 0; i--)
+            {
+                Console.Write(word = words[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
