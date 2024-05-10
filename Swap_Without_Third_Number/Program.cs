c9f87b41bee85084105112a2123c603d4ae03411
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-10 14:20:47 +0530

    Add files via upload

---
+++ Swap_Without_Third_Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Swap_Without_Third_Number
+{
+    class Program
+    {
+        //SwapWithout third Variable//
+        public void SwapWithoutThirdVariable()
+        {
+            Console.WriteLine("Enter number1");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number2");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            a = a + b;//10+20=30
+            b = a - b;//30-20=10
+            a = a - b;//30-10=20 
+
+            Console.WriteLine(a + " " + b);
+        }
+
+    }
+}
