4d13cc8cad04ea9fb21328c21606c7185d749604
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-05-10 14:20:05 +0530

    Add files via upload

---
+++ Swap_With_Third_Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Swap_With_Third_Number
+{
+    class Program
+    {
+        // Swap With Third Variable
+        public void WithThirdVariable()
+        {
+
+            Console.WriteLine("Enter number1");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number2");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int temp = 0;
+
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
